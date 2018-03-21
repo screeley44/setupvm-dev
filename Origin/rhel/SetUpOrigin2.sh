3f3dbaab6603e4f6adc8793eca0ad65dfee80262
Scott Creeley <screeley@redhat.com>
2018-03-21 18:16:49 -0400

    updates

--- Origin/rhel/SetUpOrigin2.sh
+++ Origin/rhel/SetUpOrigin2.sh
@@ -18,6 +18,32 @@ fi
 source $CONFIG_HOME/setupvm.config
 SUDO=""
 
+DoBlock()
+{
+  $SUDO lsblk
+  echo "Based on output above, what block device should the registry be set up on?"
+  read block_device
+  if [ "$block_device" == "" ]
+  then
+    echo "no block device entered, default $DEFAULT_BLOCK will be used"
+    $SUDO sh -c "echo 'DEVS=$DEFAULT_BLOCK' >> /etc/sysconfig/docker-storage-setup"
+    $SUDO sh -c "echo 'VG=$VG' >> /etc/sysconfig/docker-storage-setup"
+  else
+    echo "block device /dev/$block_device will be used, is this acceptable? (y/n)"
+    read isaccepted
+    if [ "$isaccepted" == "$yval1" ] || [ "$isaccepted" == "$yval2" ]
+    then
+      $SUDO sh -c "echo 'DEVS=/dev/$block_device' >> /etc/sysconfig/docker-storage-setup"
+      $SUDO sh -c "echo 'VG=$VG' >> /etc/sysconfig/docker-storage-setup"
+      echo "docker-storage-setup file updated"
+    else
+      echo "Let's try again..."
+      echo ""
+      DoBlock
+    fi
+  fi
+}
+
 #Perform Basic Host Configuration
 source $CONFIG_HOME/../../lib/host-config.sh
 
@@ -62,15 +88,6 @@ source $CONFIG_HOME/../../lib/post-install.sh
   # This is a common issue I've run into 
   # export PATH=$PATH:$GOPATH/bin; go get -u github.com/cloudflare/cfssl/cmd/...
 
-  echo ""
-  echo " *********************************************** "
-  echo "" 
-  echo "     RHEL Origin Setup Completed on host $HOSTNAME!"
-  echo ""
-  echo " *********************************************** "
-
-
-fi
 
 echo ""
 echo " *********************************************** "
