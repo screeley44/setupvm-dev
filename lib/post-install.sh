79e688a61ff65e785a65efb4066d81086397b8fd
Scott Creeley <screeley@redhat.com>
2018-09-14 14:15:00 -0400

    update 12

--- lib/post-install.sh
+++ lib/post-install.sh
@@ -6,35 +6,35 @@
   BASEDIR=`dirname $BASH_SOURCE`
   echo " ... ... Copying Dev Configs to Working Dir"
   echo " ... ... ... Base Dir is $BASEDIR"
-  $SUDO cp $INSTALLDIR/setupvm-dev/CNV/Origin/config-ocp.sh $KUBEPATH
-  $SUDO cp $INSTALLDIR/setupvm-dev/CNV/Origin/aws-hosts /root
-  $SUDO cp $INSTALLDIR/setupvm-dev/CNV/yaml/* $KUBEPATH/dev-configs/cinder
+  $SUDO cp $BASEDIR/../CNV/Origin/config-ocp.sh $KUBEPATH
+  $SUDO cp $BASEDIR/../CNV/Origin/aws-hosts /root
+  $SUDO cp $BASEDIR/../CNV/yaml/* $KUBEPATH/dev-configs/cinder
   $SUDO cp $BASEDIR/../yaml/aws/* $KUBEPATH/dev-configs/aws
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/gce/* $KUBEPATH/dev-configs/gce
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/hostpath/* $KUBEPATH/dev-configs/hostpath
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/nfs/* $KUBEPATH/dev-configs/nfs
-  $SUDO cp -R $INSTALLDIR/setupvm-dev/CNV/AWS/yaml/* $KUBEPATH/dev-configs/cnv/aws
-  $SUDO cp -R $INSTALLDIR/setupvm-dev/CNV/LocalVM/yaml/* $KUBEPATH/dev-configs/cnv/local
+  $SUDO cp $BASEDIR/../yaml/gce/* $KUBEPATH/dev-configs/gce
+  $SUDO cp $BASEDIR/../yaml/hostpath/* $KUBEPATH/dev-configs/hostpath
+  $SUDO cp $BASEDIR/../yaml/nfs/* $KUBEPATH/dev-configs/nfs
+  $SUDO cp -R $BASEDIR/../CNV/* $KUBEPATH/dev-configs/cnv/aws
+  $SUDO cp -R $BASEDIR/../CNV/LocalVM/yaml/* $KUBEPATH/dev-configs/cnv/local
   # $SUDO cp /root/containerized-data-importer/manifests/importer/* $KUBEPATH/dev-configs/data-importer
   # $SUDO cp /root/containerized-data-importer/manifests/importer/* $KUBEPATH/dev-configs/cdi
   $SUDO cp /root/containerized-data-importer/manifests/generated/* $KUBEPATH/dev-configs/cdi
   $SUDO cp /root/containerized-data-importer/manifests/example/* $KUBEPATH/dev-configs/cdi
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/cdi/* $KUBEPATH/dev-configs/cdi
+  $SUDO cp $BASEDIR/../yaml/cdi/* $KUBEPATH/dev-configs/cdi
   
-  $SUDO cp $INSTALLDIR/setupvm-dev/CNV/Origin/config-ocp.sh $OSEPATH
-  $SUDO cp $INSTALLDIR/setupvm-dev/CNV/Origin/aws-hosts /root
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/* $OSEPATH/dev-configs/cinder
-  $SUDO cp $BASEDIR/../yaml/aws/* $KUBEPATH/dev-configs/aws
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/gce/* $OSEPATH/dev-configs/gce
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/hostpath/* $OSEPATH/dev-configs/hostpath
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/nfs/* $OSEPATH/dev-configs/nfs
-  $SUDO cp -R $INSTALLDIR/setupvm-dev/CNV/AWS/yaml/* $OSEPATH/dev-configs/cnv/aws
-  $SUDO cp -R $INSTALLDIR/setupvm-dev/CNV/LocalVM/yaml/* $OSEPATH/dev-configs/cnv/local
+  $SUDO cp $BASEDIR/../CNV/Origin/config-ocp.sh $OSEPATH
+  $SUDO cp $BASEDIR/../CNV/Origin/aws-hosts /root
+  $SUDO cp $BASEDIR/../CNV/yaml/* $OSEPATH/dev-configs/cinder
+  $SUDO cp $BASEDIR/../yaml/aws/* $OSEPATH/dev-configs/aws
+  $SUDO cp $BASEDIR/../yaml/gce/* $OSEPATH/dev-configs/gce
+  $SUDO cp $BASEDIR/../yaml/hostpath/* $OSEPATH/dev-configs/hostpath
+  $SUDO cp $BASEDIR/../yaml/nfs/* $OSEPATH/dev-configs/nfs
+  $SUDO cp -R $BASEDIR/../CNV/* $OSEPATH/dev-configs/cnv/aws
+  $SUDO cp -R $BASEDIR/../CNV/LocalVM/yaml/* $OSEPATH/dev-configs/cnv/local
   # $SUDO cp /root/containerized-data-importer/manifests/importer/* $OSEPATH/dev-configs/data-importer
   # $SUDO cp /root/containerized-data-importer/manifests/importer/* $OSEPATH/dev-configs/cdi
   $SUDO cp /root/containerized-data-importer/manifests/generated/* $OSEPATH/dev-configs/cdi
   $SUDO cp /root/containerized-data-importer/manifests/example/* $OSEPATH/dev-configs/cdi
-  $SUDO cp $INSTALLDIR/setupvm-dev/yaml/cdi/* $OSEPATH/dev-configs/cdi
+  $SUDO cp $BASEDIR/../yaml/cdi/* $OSEPATH/dev-configs/cdi
 
   $SUDO cp host-files/* /root
 
